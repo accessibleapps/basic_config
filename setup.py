c360873eddbd34f409aed9fb02a279ed721451a5
Q <q@qwitter-client.net>
2011-07-11 21:58:37 -0400

    Initial commit creating a standard config package for all my packages

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+__version__ = 0.1
+__doc__ = """Standard configuration setup for Q-Continuum packages"""
+
+setup(
+ name = "basic_config",
+ version = __version__,
+ description = __doc__,
+ package_dir = {'basic_config': '.'},
+ packages = ['basic_config',],
+ classifiers = [
+  'Development Status :: 3 - Alpha',
+  'Intended Audience :: Developers',
+  'Programming Language :: Python',
+  'License :: OSI Approved :: MIT License',
+  'Topic :: Software Development :: Libraries',
+ ],
+)
