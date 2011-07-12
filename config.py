3c75417ad59bf462c3a8ed3ae967a5ffa18b0b8a
Q <q@qwitter-client.net>
2011-07-11 22:05:39 -0400

    Minor syntax correction

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from validate import Validator, ValidateError
 import os
 from platform_utils import paths
 
-def load_config(config_path, confspec_path, ):
+def load_config(config_path, confspec_path):
  fullpath = os.path.join(paths.app_data_path(app_name), config_path)
  config = ConfigObj(infile=fullpath, configspec=configspec_path, create_empty=True, stringify=True)
  validator = Validator()
