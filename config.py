41a20dcd5b4810ed79fd5676f899697dc3d6f484
Q <q@qwitter-client.net>
2011-07-12 01:00:36 -0400

    Renamed load_config to load_configuration

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from validate import Validator, ValidateError
 import os
 from platform_utils import paths
 
-def load_configuration(config_path, confspec_path):
+def load_configuration(config_path, configspec_path):
  config = ConfigObj(infile=config_path, configspec=configspec_path, create_empty=True, stringify=True)
  validator = Validator()
  validated = config.validate(validator, copy=True)
