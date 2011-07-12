c7bdee2efa789ed05e0aedd67e6e8d9c48fc9a77
Q <q@qwitter-client.net>
2011-07-11 22:11:45 -0400

    Fix to load from paths

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ import os
 from platform_utils import paths
 
 def load_configuration(config_path, confspec_path):
- fullpath = os.path.join(paths.app_data_path(app_name), config_path)
- config = ConfigObj(infile=fullpath, configspec=configspec_path, create_empty=True, stringify=True)
+ config = ConfigObj(infile=config_path, configspec=configspec_path, create_empty=True, stringify=True)
  validator = Validator()
  validated = config.validate(validator, copy=True)
  if validated:
