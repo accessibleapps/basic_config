d796ee20171be8d2135c70d2f72b8bcdeff57df8
Christopher Toth <q.alpha@gmail.com>
2011-07-13 22:50:59 -0400

    Allow passing extra arguments to the configuration process

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from validate import Validator, ValidateError
 import os
 from platform_utils import paths
 
-def load_configuration(config_path, configspec_path):
- config = ConfigObj(infile=config_path, configspec=configspec_path, create_empty=True, stringify=True)
+def load_configuration(config_path, configspec_path, *args, **kwargs):
+ config = ConfigObj(infile=config_path, configspec=configspec_path, create_empty=True, *args, **kwargs)
  validator = Validator()
  validated = config.validate(validator, copy=True)
  if validated:
