1ab0c738b972cb034bdf262a325986cd5052198b
Q <q@qwitter-client.net>
2011-11-09 14:32:10 -0500

    This seems to work better

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from configobj import ConfigObj
 from validate import Validator, ValidateError
 import os
 def load_configuration(config_path, configspec_path, *args, **kwargs):
- spec = ConfigObj(configspec_path, encoding='UTF8')
+ spec = ConfigObj(configspec_path, encoding='UTF8', _inspec=True)
  config = ConfigObj(infile=config_path, configspec=spec, create_empty=True, encoding='UTF8', *args, **kwargs)
  validator = Validator()
  validated = config.validate(validator, copy=True)
