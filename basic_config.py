b1a647e8f101dbe4edf6d6e0603fa63e3489ec49
Q <q@qwitter-client.net>
2012-12-09 06:26:36 -0700

    Minor cleanup

--- basic_config.py
+++ basic_config.py
@@ -1,11 +1,12 @@
 from configobj import ConfigObj
 from validate import Validator, ValidateError
 import os
+
 def load_configuration(config_path, configspec_path, *args, **kwargs):
- spec = ConfigObj(configspec_path, encoding='UTF8', _inspec=True)
+ spec = ConfigObj(configspec_path, encoding='UTF8', list_values=False, _inspec=True)
  config = ConfigObj(infile=config_path, configspec=spec, create_empty=True, encoding='UTF8', *args, **kwargs)
  validator = Validator()
  validated = config.validate(validator, copy=True)
- if validated:
+ if validated == True:
   config.write()
   return config
