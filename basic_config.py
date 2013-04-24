bd9078d91aa9e0f34d483aad09932a77ffbf6fe8
Q <q@q-continuum.net>
2013-04-23 18:47:12 -0600

    Make configspec_path optional

--- basic_config.py
+++ basic_config.py
@@ -2,7 +2,7 @@ from configobj import ConfigObj
 from validate import Validator, ValidateError
 import os
 
-def load_configuration(config_path, configspec_path, *args, **kwargs):
+def load_configuration(config_path, configspec_path=None, *args, **kwargs):
  spec = ConfigObj(configspec_path, encoding='UTF8', list_values=False, _inspec=True)
  config = ConfigObj(infile=config_path, configspec=spec, create_empty=True, encoding='UTF8', *args, **kwargs)
  validator = Validator()
