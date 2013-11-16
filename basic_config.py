79aab87ba24207dd5a4f77b2352f347679910167
Q <q@q-continuum.net>
2013-11-16 12:12:50 -0700

    Raise an error if we fail to load the config, a ConfigLoadError to be precise

--- basic_config.py
+++ basic_config.py
@@ -1,10 +1,16 @@
-from configobj import ConfigObj
+from configobj import ConfigObj, ParseError
 from validate import Validator, ValidateError
 import os
 
+class ConfigLoadError(Exception): pass
+
+
 def load_configuration(config_path, configspec_path=None, *args, **kwargs):
  spec = ConfigObj(configspec_path, encoding='UTF8', list_values=False, _inspec=True)
- config = ConfigObj(infile=config_path, configspec=spec, create_empty=True, encoding='UTF8', *args, **kwargs)
+ try:
+  config = ConfigObj(infile=config_path, configspec=spec, create_empty=True, encoding='UTF8', *args, **kwargs)
+ except ParseError:
+  raise ConfigLoadError("Unable to load %r" % config_path)
  validator = Validator()
  validated = config.validate(validator, copy=True)
  if validated == True:
